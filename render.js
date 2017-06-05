eee2af44d8ece74ecccd2d09ab78f57144eb5259
Ciao <ciao0958@gmail.com>
2017-06-06 00:24:42 +0800

    dev

--- render.js
+++ render.js
@@ -1,12 +1,19 @@
 var webPage = require('webpage');
+var fs = require('fs');
+
 var page = webPage.create();
 var url = 'https://mei-stylist.idv.tw';
+var outputPath = fs.workingDirectory+'/render.json';
 
 page.viewportSize = { width: 1920, height: 1080 };
 page.open(url, function (status) {
   setTimeout(function() {
-  	page.render('capture.jpg', {format: 'jpg', quality: '100'});
-  	console.log(page.content)
-  	phantom.exit();
+	page.render('capture.jpg', {format: 'jpg', quality: '100'});
+
+	var data = JSON.stringify({
+	  content: page.content,
+	});
+	fs.writeFile(outputPath, 'data', 'w')
+	phantom.exit();
   }, 3000);
 });
