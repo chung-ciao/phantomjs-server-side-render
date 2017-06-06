ac100c91b5816654773f40286a817d7a812d2dce
Ciao <ciao0958@gmail.com>
2017-06-06 13:19:20 +0800

    create render.json

--- render.js
+++ render.js
@@ -2,18 +2,21 @@ var webPage = require('webpage');
 var fs = require('fs');
 
 var page = webPage.create();
-var url = 'https://mei-stylist.idv.tw';
+var url = 'http://localhost:8083';
 var outputPath = fs.workingDirectory+'/render.json';
-
+var render = function() {
+  page.render('capture.jpg', {format: 'jpg', quality: '100'});
+  console.log(page.content)
+  fs.write(outputPath, JSON.stringify({
+    content: page.content,
+  }), 'w')
+  phantom.exit();
+}
 page.viewportSize = { width: 1920, height: 1080 };
 page.open(url, function (status) {
-  setTimeout(function() {
-	page.render('capture.jpg', {format: 'jpg', quality: '100'});
-
-	var data = JSON.stringify({
-	  content: page.content,
-	});
-	fs.writeFile(outputPath, 'data', 'w')
-	phantom.exit();
-  }, 3000);
+  page.onCallback = function() {
+    setTimeout(function() {
+      render()
+    }, 500);
+  }
 });
