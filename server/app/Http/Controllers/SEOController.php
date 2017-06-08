5396f86d75e6563a6590e08e800e0b5aa00cb70b
Ciao <ciao0958@gmail.com>
2017-06-08 09:21:34 +0800

    use symfony process execute phantomjs

--- server/app/Http/Controllers/SEOController.php
+++ server/app/Http/Controllers/SEOController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class SEOController extends Controller
 {
-    public function render(Request $request) {
-    	$url = $request->url;
-        $crawlerPath = resource_path('crawler/render.js');
-        $crawlerFolder = resource_path('crawler');
+  public function render(Request $request) {
+  	$url = $request->url;
 
-       	$shell_output = shell_exec("cd $crawlerFolder; phantomjs render.js $url;");
-       	$renderResult = file_get_contents(resource_path('crawler/render.json'));
-        return json_encode($renderResult);
-    }
+    // 執行phantomjs爬頁面
+    $crawlerPath = resource_path('crawler/render.js');
+    $process = new Process("phantomjs $crawlerPath $url");
+		$process->run();
+
+    // 讀取phantomjs爬出的內容並回傳
+   	$renderResult = file_get_contents(public_path('render.json'));
+    return json_encode($renderResult);
+  }
 }
