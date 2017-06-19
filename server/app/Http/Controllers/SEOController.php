5a5fab45842200e8311cd4c00600581831affd62
Ciao <ciao0958@gmail.com>
2017-06-19 21:50:13 +0800

    dev

--- server/app/Http/Controllers/SEOController.php
+++ server/app/Http/Controllers/SEOController.php
@@ -10,53 +10,63 @@ use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
 class SEOController extends Controller
 {
-  protected $phantomConfig;
+	protected $phantomConfig;
 
-  public function __construct() {
-    $phantomConfig = "--load-images=false ";
-    $phantomConfig .=  "--proxy-type=none ";
-    $phantomConfig .=  "--ignore-ssl-errors=true ";
-    $phantomConfig .=  "--load-images=false ";
-    $phantomConfig .=  "--local-to-remote-url-access=true";
-    $this->phantomConfig = $phantomConfig;
-  }
+	public function __construct() {
+		$phantomConfig = "--load-images=false ";
+		$phantomConfig .= "--proxy-type=none ";
+		$phantomConfig .= "--ignore-ssl-errors=true ";
+		$phantomConfig .= "--load-images=false ";
+		$phantomConfig .= "--local-to-remote-url-access=true";
+		$this->phantomConfig = $phantomConfig;
+		$this->tempFile = "/tmp/render-".timestamp_micro().".json";
+	}
 
-  public function render(Request $request) {
-  	$url = $request->url;
+	public function render(Request $request) {
+		$url = $request->url;
 
-    // 確認是否有cache
-    if($this->getCache($url) != false) {
-      return json_encode($this->getCache($url));
-    }
+		// 確認是否有cache
+		if($this->getCache($url) != false) {
+			return json_encode($this->getCache($url));
+		}
 
-    // 執行phantomjs爬頁面
-    $crawler = resource_path('crawler/render.js');
-    $outputPath = 'tmp/render.json';
-    $phantomConfig = $this->phantomConfig;
-    $process = new Process("phantomjs $phantomConfig $crawler $url $outputPath");
-    $process->run();
+		// 執行phantomjs爬頁面
+		$crawler = resource_path('crawler/render.js');
+		$phantomConfig = $this->phantomConfig;
+		$process = new Process("phantomjs $phantomConfig $crawler $url $this->tempFile");
+		$process->run();
+		Log::info($process->getOutput());
+		
+		if (!$process->isSuccessful()) {
+			return json_encode([
+				'status' => 404
+			]);
+		}
 
-    if (!$process->isSuccessful()) {
-      return json_encode([
-        'status' => 404
-      ]);
-    }
+		// 讀取phantomjs爬出的內容回傳, 並寫回cache
+	 	$renderResult = json_decode(file_get_contents($this->tempFile));
+		$this->addCache($url, $renderResult);
 
-    // 讀取phantomjs爬出的內容回傳, 並寫回cache
-   	$renderResult = json_decode(file_get_contents('tmp/render.json'));
-    $this->addCache($url, $renderResult);
-    return json_encode($renderResult);
-  }
+		// if($renderResult->status == 200) {
+		// 	echo $renderResult->content;
+		// }
+		// else {
+		// 	echo $process->getOutput()."<br><br>";
+		// 	echo '404';
+		// }
+		@unlink($this->tempFile);
+		return json_encode($renderResult);
+	}
 
-  protected function getCache($key) {
-    $data = Cache::get($key);
-    if($data == null) return false;
-    return $data;
-  }
+	protected function getCache($key) {
+		$data = Cache::get($key);
+		if($data == null) return false;
+		return $data;
+	}
 
-  protected function addCache($key, $data) {
-    $expireAt = Carbon::now()->addHours(6);
-    Cache::add($key, $data, $expireAt);
-    return;
-  }
+	protected function addCache($key, $data) {
+		$expireAt = Carbon::now()->addHours(6);
+		Cache::add($key, $data, $expireAt);
+		return;
+	}
 }
