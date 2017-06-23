d0c15413b4980732c2034c5f146d0d2f2e6e7482
Ciao <ciao0958@gmail.com>
2017-06-23 13:25:27 +0800

    add 404 view

---
+++ server/resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>
+                <label class="label label-danger">
+                    404 Not Found
+                </label>
+            </h1>
+        </div>
+    </div>
+</div>
+</body>
+</html>
